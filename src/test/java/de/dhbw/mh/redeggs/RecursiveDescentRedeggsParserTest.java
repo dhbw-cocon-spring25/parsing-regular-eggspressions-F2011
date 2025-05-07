f93e3d18a8a97b223c89616fb3e64f99dd03f365
I589151 <leon.fertig@sap.com>
2025-05-07 09:50:56 +0200

    Add tests

--- src/test/java/de/dhbw/mh/redeggs/RecursiveDescentRedeggsParserTest.java
+++ src/test/java/de/dhbw/mh/redeggs/RecursiveDescentRedeggsParserTest.java
@@ -4,12 +4,17 @@ import static de.dhbw.mh.redeggs.CodePointRange.range;
 import static de.dhbw.mh.redeggs.CodePointRange.single;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.junit.jupiter.api.Assertions.fail;
 
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Random;
+import java.util.Set;
 import java.util.stream.Collectors;
 
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import de.dhbw.mh.redeggs.RegularEggspression.Alternation;
@@ -173,25 +178,6 @@ public class RecursiveDescentRedeggsParserTest {
     }
 
     @Test
-    public void missingRightParenThrowsException() throws RedeggsParseException {
-        String input = "((e)";
-
-        assertThatExceptionOfType(RedeggsParseException.class)
-                .as("Unbalanced parentheses should raise a RedeggsParseException.")
-                .isThrownBy(() -> parser.parse(input))
-                .withMessage("Input ended unexpectedly, expected symbol ')' at position 5.");
-    }
-
-    @Test
-    public void unbalancedRightParenThrowsException() {
-        String input = "((a)))";
-
-        assertThatExceptionOfType(RedeggsParseException.class)
-                .as("Unbalanced parentheses should raise a RedeggsParseException.")
-                .isThrownBy(() -> parser.parse(input)).withMessage("Unexpected symbol ')' at position 6.");
-    }
-
-    @Test
     public void randomTest() throws RedeggsParseException {
         RegularEggspression expr = parser.parse("([0-9a-fA-F]|[Yyz+])*");
 
@@ -204,6 +190,177 @@ public class RecursiveDescentRedeggsParserTest {
                                 .toString(),
                         SYMBOL_FACTORY.newSymbol().include(single('Y'), single('y'), single('z'), single('+'))
                                 .andNothingElse().toString()));
+    }
+
+    public static final Random RANDOM = new Random();
+
+    private static final Set<Integer> EXCLUDED_CODEPOINTS = Set.of((int) '*', (int) '+', (int) '.', (int) '|',
+            (int) '(', (int) ')', (int) '[', (int) ']', (int) 'ε', (int) '∅');
+
+    public static Integer[] getRandomCodepoints(int number) {
+        int maxCodePoint = 0xffff;
+        Integer[] result = new Integer[number];
+        int count = 0;
+
+        while (count < number) {
+            int cp = 33 + RANDOM.nextInt(maxCodePoint - 33 + 1);
+
+            if (!EXCLUDED_CODEPOINTS.contains(cp) && Character.isDefined(cp)) {
+                result[count++] = cp;
+            }
+        }
+
+        return result;
+    }
+
+    public static String join(int[] array, String separator) {
+        if (array.length == 0) {
+            return "";
+        }
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < array.length - 1; i++) {
+            stringBuilder.appendCodePoint(array[i]);
+            stringBuilder.append(separator);
+        }
+        stringBuilder.appendCodePoint(array[array.length - 1]);
+        return stringBuilder.toString();
+    }
+
+    @Test
+    @Tag("associativity")
+    public void concatenationIsLeftAssociative() throws Exception {
+        Object[] randomChars = getRandomCodepoints(3);
+        String input = String.format("%c%c%c", randomChars);
+
+        RegularEggspression expr = parser.parse(input);
+
+        String expectation = String.format("(([\\u%04X][\\u%04X])[\\u%04X])", randomChars);
+        String result = expr.accept(INSPECTOR);
+        if (!result.equals(expectation)) {
+            fail("Concatenation is not left-associative.");
+        }
+    }
+
+    @Test
+    @Tag("associativity")
+    public void alternationIsLeftAssociative() throws Exception {
+        Object[] randomChars = getRandomCodepoints(3);
+        String input = String.format("%c|%c|%c", randomChars);
+
+        RegularEggspression expr = parser.parse(input);
+
+        String expectation = String.format("(([\\u%04X]|[\\u%04X])|[\\u%04X])", randomChars);
+        String result = expr.accept(INSPECTOR);
+        if (!result.equals(expectation)) {
+            fail("Alternation is not left-associative.");
+        }
+    }
+
+    @Test
+    @Tag("precedence")
+    public void concatenationTakesPrecedence() throws RedeggsParseException {
+        Object[] randomChars = getRandomCodepoints(3);
+        String input = String.format("%c|%c%c", randomChars);
+
+        RegularEggspression expr = parser.parse(input);
+
+        String expectation = String.format("([\\u%04X]|([\\u%04X][\\u%04X]))", randomChars);
+        String result = expr.accept(INSPECTOR);
+        if (!result.equals(expectation)) {
+            fail("Concatenation yields precedence to alternation.");
+        }
+    }
+
+    @Test
+    @Tag("precedence")
+    public void kleeneStarTakesPrecedence() throws RedeggsParseException {
+        Object[] randomChars = getRandomCodepoints(2);
+        String input = String.format("%c%c*", randomChars);
+
+        RegularEggspression expr = parser.parse(input);
+
+        String expectation = String.format("([\\u%04X]([\\u%04X])*)", randomChars);
+        String result = expr.accept(INSPECTOR);
+        if (!result.equals(expectation)) {
+            fail("Concatenation yields precedence to alternation.");
+        }
+    }
+
+    @Test
+    @Tag("character-classes")
+    public void charClasAcceptsMultipleCodepoints() throws RedeggsParseException {
+        Object[] randomChars = getRandomCodepoints(3);
+        String input = String.format("[%c%c%c]", randomChars);
+
+        RegularEggspression expr = parser.parse(input);
+
+        Arrays.sort(randomChars);
+        String expectation = String.format("[\\u%04X\\u%04X\\u%04X]", randomChars);
+        String result = expr.accept(INSPECTOR);
+        if (!result.equals(expectation)) {
+            fail("Concatenation yields precedence to alternation.");
+        }
+    }
+
+    @Test
+    @Tag("character-classes")
+    public void leftBracketCannotBeNested() throws RedeggsParseException {
+        String input = "[[]";
+
+        RegularEggspression expr = parser.parse(input);
+
+        String expectation = String.format("[\\u%04X]", (int) '[');
+        String result = expr.accept(INSPECTOR);
+        if (!result.equals(expectation)) {
+            fail("Character classes cannot be nested.");
+        }
+    }
+
+    @Test
+    @Tag("edge-cases")
+    public void dashHasNoFunctionOutsideCharClass() throws RedeggsParseException {
+        String input = "a-z";
+
+        RegularEggspression expr = parser.parse(input);
+
+        String expectation = String.format("(([\\u%04X][\\u%04X])[\\u%04X])", (int) 'a', (int) '-', (int) 'z');
+        String result = expr.accept(INSPECTOR);
+        if (!result.equals(expectation)) {
+            fail("Intervals do not exist outside of character classes.");
+        }
+    }
+
+    @Test
+    @Tag("parentheses")
+    public void doublyClosedCharClassThrowsException() throws RedeggsParseException {
+        String input = "[[]]";
+
+        assertThatExceptionOfType(RedeggsParseException.class)
+                .as("Doubly closed character class should raise a RedeggsParseException.")
+                .isThrownBy(() -> parser.parse(input)).withMessage("Unexpected symbol '%c' at position %d.", ']', 4);
+    }
+
+    @Test
+    @Tag("parentheses")
+    public void missingRightParenThrowsException() throws RedeggsParseException {
+        Object[] randomChars = getRandomCodepoints(1);
+        String input = String.format("((%c)", randomChars);
+
+        assertThatExceptionOfType(RedeggsParseException.class)
+                .as("Unbalanced parentheses should raise a RedeggsParseException.")
+                .isThrownBy(() -> parser.parse(input))
+                .withMessage("Input ended unexpectedly, expected symbol '%c' at position %d.", ')', 5);
+    }
+
+    @Test
+    @Tag("parentheses")
+    public void unbalancedRightParenThrowsException() throws RedeggsParseException {
+        Object[] randomChars = getRandomCodepoints(1);
+        String input = String.format("((%c)))", randomChars);
+
+        assertThatExceptionOfType(RedeggsParseException.class)
+                .as("Unbalanced parentheses should raise a RedeggsParseException.")
+                .isThrownBy(() -> parser.parse(input)).withMessage("Unexpected symbol '%c' at position %d.", ')', 6);
     }
 
 }
